f64893a2f87b8e7130c80f9b5b1441145f53694f
minht11 <justas543@gmail.com>
2021-09-14 10:36:32 +0300

    Implement console ui

--- CfgComparator/Program.cs
+++ CfgComparator/Program.cs
@@ -7,15 +7,56 @@ namespace CfgComparator
     {
         static void Main(string[] args)
         {
-            string sourcePath = "./test-data/FMB920-default.cfg";
-            string targetPath = "./test-data/FMB920-modified.cfg";
+            const string unchanged = "-u";
+            const string added = "-a";
+            const string removed = "-r";
+            const string modified = "-m";
+            const string starts = "--starts=";
+
+            Console.WriteLine("Input source and target file locations");
+            Console.WriteLine("Options:");
+            Console.WriteLine($"{unchanged} : show unchanged");
+            Console.WriteLine($"{added} : show added");
+            Console.WriteLine($"{removed} : show removed");
+            Console.WriteLine($"{modified} : show modified");
+            Console.WriteLine($"{starts}* : keys should start with *");
+
+            string line = Console.ReadLine();
+            if (line == "" || line == null) {
+                return;
+            }
+
+            var input = new List<string>(line.Split(' '));
+            // string sourcePath = "./test-data/FMB920-default.cfg";
+            // string targetPath = "./test-data/FMB920-modified.cfg";
+            var sourcePath = input?[0];
+            var targetPath = input?[1];
+            input.RemoveAt(0);
+            input.RemoveAt(0);
+
+            if (sourcePath == null || targetPath == null)
+            {
+                Console.WriteLine("Source and target paths cannot be empty");
+                return;
+            }
+
+            bool showUnchanged = input.Contains(unchanged);
+            bool showAdded = input.Contains(added);
+            bool showRemoved = input.Contains(removed);
+            bool showModified = input.Contains(modified);
+            string startsWithOption = input.Find((value) => value.StartsWith(starts));
+            string startsValue = "";
+            if (startsWithOption != null)
+            {
+                startsValue = startsWithOption.Split('=')?[1];
+            }
 
             CfgReader reader = new();
             var source = reader.Read(sourcePath);
             var target = reader.Read(targetPath);
             var analysis = CfgAnalysis.Analyse(source, target);
-
-            Display(analysis, true, true, true, true, "");
+            
+            Display(analysis, showUnchanged, showModified, showAdded, showRemoved, startsValue);
         }
 
         static void DisplaySection(Dictionary<int, string> data, string title, bool show, string keyStarts) {
@@ -43,7 +84,7 @@ namespace CfgComparator
             DisplaySection(analysis.Unchanged, "Unchanged:", showUnchanged, keyStarts);
             DisplaySection(analysis.Added, "Added:", showAdded, keyStarts);
             DisplaySection(analysis.Removed, "Removed:", showRemoved, keyStarts);
-            DisplaySection(analysis.Modified, "Modified:", showRemoved, keyStarts, (value) => $"{value.Item1} -> {value.Item2}");
+            DisplaySection(analysis.Modified, "Modified:", showModified, keyStarts, (value) => $"{value.Item1} -> {value.Item2}");
         }
     }
 }
