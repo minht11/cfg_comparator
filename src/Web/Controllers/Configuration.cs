76527b2f63f1fb160f281f965c18a0ed83a972bb
minht11 <justas543@gmail.com>
2021-10-08 14:12:40 +0300

    Implement filtering

--- src/Web/Controllers/Configuration.cs
+++ src/Web/Controllers/Configuration.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
+using System.Linq;
 using System;
 using Web.Models;
 using Cfg.Configuration;
@@ -26,9 +27,40 @@ namespace Web.Controllers
             var sourceRecord = Reader.Read(sourceFile.OpenReadStream(), sourceFile.FileName);
             var targetRecord = Reader.Read(targetFile.OpenReadStream(), targetFile.FileName);
 
-            var analysis = Analyzer.Compare(sourceRecord, targetRecord);
+            var comparedParams = Analyzer.Compare(sourceRecord, targetRecord);
+            var filteredParams = FilterByQuery(comparedParams, query);
 
-            return Ok(await Task.FromResult(analysis));
+            return Ok(await Task.FromResult(filteredParams));
+        }
+
+        private static List<ComparedParameter> FilterByQuery(List<ComparedParameter> parameters, CompareQuery query)
+        {
+            var idStarts = query.IdStartsWith;
+            var shouldNotFilterById = string.IsNullOrEmpty(idStarts);
+            var shouldNotFilterByStatus = (
+                query.Unchanged == null
+                && query.Modified == null
+                && query.Added == null
+                && query.Removed == null
+            );
+
+            return parameters.Where((param) => {
+                var visibleStatus = true;
+                if (!shouldNotFilterByStatus)
+                {
+                    visibleStatus = param.Status switch
+                    {
+                        ComparisonStatus.Unchanged => query.Unchanged,
+                        ComparisonStatus.Modified => query.Modified,
+                        ComparisonStatus.Added => query.Added,
+                        ComparisonStatus.Removed => query.Removed,
+                        _ => false,
+                    } ?? false;
+                }
+      
+                return visibleStatus && (shouldNotFilterById || param.ID.StartsWith(idStarts!));
+
+            }).ToList();
         }
     }
 }
