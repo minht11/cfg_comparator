d6f25d300ebb030b671f736249c56340446682d0
minht11 <justas543@gmail.com>
2021-10-08 11:23:42 +0300

    Setup configuration controller

--- src/Web/Controllers/Configuration.cs
+++ src/Web/Controllers/Configuration.cs
@@ -1,20 +1,28 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-// namespace Web.Controllers
-// {
-//     public class person
-//     {
-//         public string name { get; set; }
-//         public string surname { get; set; }
-//     }
-//     public class personController : ApiController
-//     {
-//         [HttpPost]
-//         public void Post([FromBody] person p)
-//         {
-//         }
-//     }
-// }
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("configuration")]
+    public class Configuration : ControllerBase
+    {
+        private readonly ILogger<Configuration> _logger;
+
+        public Configuration(ILogger<Configuration> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("compare")]
+        public string Post()
+        {
+            return "Compare";
+        }
+    }
+}
