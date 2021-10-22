2831d46462808e6aed07cf0a419d8c543d182465
minht11 <justas543@gmail.com>
2021-10-22 13:54:55 +0300

    Change setters to be init

--- src/Web/Models/ReaderInput.cs
+++ src/Web/Models/ReaderInput.cs
@@ -5,12 +5,12 @@ namespace Web.Models
 {
     public class ReaderInput
     {
-        public string? SourcePath { get; set; }
+        public string? SourcePath { get; init; }
 
-        public string? TargetPath { get; set; }
+        public string? TargetPath { get; init; }
 
-        public List<string>? FilterByStatus { get; set; }
+        public List<string>? FilterByStatus { get; init; }
 
-        public string? IdStartsWith { get; set; }
+        public string? IdStartsWith { get; init; }
     }
 }
